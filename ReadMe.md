df41c2300f5243757236caf77c5717cef8163b11
Steve G. Bjorg <steve.bjorg@gmail.com>
2023-02-14 16:29:50 -0800

    update copyright year

--- ReadMe.md
+++ ReadMe.md
@@ -48,4 +48,4 @@ Console.ReadLine();
 
 This application is distributed under the GNU Affero General Public License v3.0 or later.
 
-Copyright (C) 2020-2022 - Steve G. Bjorg
+Copyright (C) 2020-2023 - Steve G. Bjorg
--- Samples/HelloTrinnov/Program.cs
+++ Samples/HelloTrinnov/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * RadiantPi.Telnet - Client for Telnet protocol
- * Copyright (C) 2020-2022 - Steve G. Bjorg
+ * Copyright (C) 2020-2023 - Steve G. Bjorg
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
